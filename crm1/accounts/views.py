79d95dec537ce1f0f5d5faf0326d9420cc5e3dc8
Toleu Bahauddin <baha200477@gmail.com>
2023-11-11 20:00:21 +0600

    push_crm_course

---
+++ crm1/accounts/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return HttpResponse("home")
+
+
+def products(request):
+    return HttpResponse("products")
+
+
+def customer(request):
+    return HttpResponse("customer")
